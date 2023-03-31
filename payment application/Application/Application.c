1755d8c4342b68ceb55098baec952e411b8f4fa9
Elpolaky <kareempolaky979@gmail.com>
2023-03-31 23:05:50 +0200

    server updated

--- payment application/Application/Application.c
+++ payment application/Application/Application.c
@@ -1,8 +1,30 @@
+#include"../Card/Card.h"
+#include"../Terminal/Terminal.h"
 
 
-void main(){
+void main()
+{
 
-  isValidAccountTest();
+/*
+//calling database and pointer to ST_cardData_t
+    _isValidAccount=isValidAccountTest(,accountsDB);
 
+    //calling database
+    _isBlockedAccount=isBlockedAccount(accountsDB);
+
+    // calling pointer to ST_terminalData_t data and database
+    _isAmountAvailable=isAmountAvailable(,accountsDB);
+
+
+    //calling database transaction
+    _saveTransaction=saveTransaction();
+*/
+
+   // recieveTransactionDataTest();
+
+  // isValidAccountTest();
+ // isBlockedAccountTest();
+ //isAmountAvailableTest();
+saveTransactionTesr();
 
 }
