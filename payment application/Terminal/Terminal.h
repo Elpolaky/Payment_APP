deecca10f8008652b677b383b7f732baa59d73c4
Elpolaky <kareempolaky979@gmail.com>
2023-04-01 03:04:06 +0200

    terminal.h updated

--- payment application/Terminal/Terminal.h
+++ payment application/Terminal/Terminal.h
@@ -1,22 +1,22 @@
 #pragma once
-
+/******************************************************************************************************************************************************************/
 #define _CRT_SECURE_NO_WARNINGS
 #define _CRT_NONSTDC_NO_WARNINGS
-
+/******************************************************************************************************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
-
+/******************************************************************************************************************************************************************/
 extern float maxAmount;
-
+/******************************************************************************************************************************************************************/
 typedef struct ST_terminalData_t
 {
     float transAmount;
     float maxTransAmount;
     uint8_t transactionDate[11];
 }ST_terminalData_t;
-
+/******************************************************************************************************************************************************************/
 typedef enum EN_terminalError_t
 {
     TERMINAL_OK,
@@ -27,23 +27,13 @@ typedef enum EN_terminalError_t
     EXCEED_MAX_AMOUNT,
     INVALID_MAX_AMOUNT
 }EN_terminalError_t;
-
-typedef struct ST_cardData_t
-{
-    uint8_t cardHolderName[25];
-    uint8_t primaryAccountNumber[20];
-    uint8_t cardExpirationDate[6];
-}ST_cardData_t;
-
+/******************************************************************************************************************************************************************/
 EN_terminalError_t getTransactionDate(ST_terminalData_t* termData);
 EN_terminalError_t isCardExpired(ST_cardData_t* cardData, ST_terminalData_t* termData);
 EN_terminalError_t getTransactionAmount(ST_terminalData_t* termData);
 EN_terminalError_t isBelowMaxAmount(ST_terminalData_t* termData);
 EN_terminalError_t setMaxAmount(ST_terminalData_t* termData, float maxAmount);
-
-
-
-
+/******************************************************************************************************************************************************************/
 void getTransactionDateTest(void);
 void isCardExpriedTest(void);
 void getTransactionAmountTest(void);
